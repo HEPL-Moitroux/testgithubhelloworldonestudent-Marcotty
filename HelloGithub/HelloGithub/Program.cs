4fa0dc39e3e7ef509b41616cfe3c35a4dc9d72eb
Marcotty <47335727+Marcotty@users.noreply.github.com>
2019-11-06 10:24:46 +0100

    Upload Labo1
    Upload de tout le projet... (ou upload du .cs seulement?)

---
+++ HelloGithub/HelloGithub/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Github from Frederic Marcotty");
+            Console.ReadLine();
+        }
+    }
+}
